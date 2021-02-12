b024a82a75ad16d12f2c63ee1054c81e52c5b20d
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-02-12 17:20:43 -0600

    tailwind footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,96 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
+import KDLogo2 from '../portfolio_ideas/KDLogo2.png';
 
-const FooterWrapper = styled.div`
 
-// MOBILE
-/* Footer Section */
-#footer {
-  background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
-}
+const Footer = () => {
 
-#footer .footer {
-  min-height: 200px;
-  flex-direction: column;
-  padding-top: 1%;
-  padding-bottom: 1%;
-}
-#footer h1 {
-  font-size: 2.5rem;
-  margin-bottom: 1%;
-}
-
-#footer h2 {
-  color: white;
-  font-weight: 500;
-  font-size: 1.5rem;
-  letter-spacing: 0.1rem;
-  margin: 1% auto;
-}
-
-#footer .social-icon {
-  display: flex;
-  margin-bottom: 10px;
-}
-
-#footer .social-item {
-  height: 55%;
-  width: 50%;
-  margin: 2%;
-}
-/* End of Footer */
-
-// TABLET
-@media only screen and (min-width: 768px) {
-/* Footer Section */
-#footer h1 {
-    font-size: 3.5rem;
-  }
-
-  #footer h2 {
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-  }
-  /* End of Footer */
-}
-
-//DESKTOP
-@media only screen and (min-width: 1200px) {
-  /* Footer Section */
-#footer h1 {
-  font-size: 4rem;
-}
-
-#footer h2 {
-  font-size: 2rem;
-}
-/* End of Footer */
-}
-`
-
-export default function Footer() {
     return (
-        // <!-- Footer -->
-      <FooterWrapper>
-        <section id="footer">
-          <div class="footer container">
-            <div class="logo"><h1><span>K</span>enan <span>D</span>olic</h1></div>
-            <h2>Connect With Me</h2>
-            <div class="social-icon">
-              <div class="social-item">
-                <a href="https://www.linkedin.com/in/kenan-dolic/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/windows/48/000000/linkedin.png" alt='linkedin'/></a>
-              </div>
-              <div class="social-item">
-                <a href="https://github.com/kdolic" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent-systems-filled/48/000000/github.png" alt='github'/></a>
-              </div>
-              <div class="social-item">
-                <a href="https://twitter.com/DolicKenan" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/material-outlined/48/000000/twitter.png" alt='twitter'/></a>
-              </div>
-            </div>
-            <h4>© Kenan Dolic 2021</h4>
+        <section className="bg-gradient-to-r from-gray-800 to-gray-700 h-80 flex justify-center">
+        <div class="h-2 flex-col items-center justify-center pt-6 pb-6 ">
+          <div class="logo flex justify-center">
+            <img className='w-20 mb-6 md:w-24' src={KDLogo2} alt="logo"/>
           </div>
-        </section>
-      </FooterWrapper>
-        // <!-- End of Footer -->
+          <h2 className='text-white text-center text-2xl font-medium tracking-wider m-2 md:text-3xl xl:text-4xl'>Connect With Me</h2>
+          <div className="flex justify-center mb-3">
+            <div className="m-2">
+              <a href="https://www.linkedin.com/in/kenan-dolic/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/windows/48/000000/linkedin.png" alt='linkedin'/></a>
+            </div>
+            <div className="m-2">
+              <a href="https://github.com/kdolic" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent-systems-filled/48/000000/github.png" alt='github'/></a>
+            </div>
+            <div className="m-2">
+              <a href="https://twitter.com/DolicKenan" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/material-outlined/48/000000/twitter.png" alt='twitter'/></a>
+            </div>
+          </div>
+          <h4 className='text-md font-bold text-center mb-10'>© Kenan Dolic 2021</h4>
+        </div>
+      </section>
     )
 }
+
+export default Footer
