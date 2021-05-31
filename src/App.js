45c880d3cb305561ecc70c0d72cf71c4c2bfe9c9
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-31 16:23:43 -0500

    portfolio update with data.json - Home complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Navbar from './components/Nav/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -6,17 +6,25 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
+import JsonData from './data/data.json'
+
 import './css/App.css'
 
 const App = () => {
+  const [pageData, setPageData] = useState({})
+
+  useEffect(() => {
+    setPageData(JsonData)
+  }, [])
+
   return (
    <div>
      <Navbar/>
-     <Home/>
-     <About/>
-     <Projects/>
-     <Contact/>
-     <Footer/>
+     <Home data={pageData.Home}/>
+     <About data={pageData.About}/>
+     <Projects data={pageData.Projects}/>
+     <Contact data={pageData.Contact}/>
+     <Footer data={pageData.Footer}/>
    </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect';
 
-const Home = () => {
+const Home = (props) => {
     return (
       <section id="home" className='bg-home'>
       <div class="max-w-7xl mx-auto px-4 min-h-screen w-full flex justify-start items-center">
@@ -9,7 +9,7 @@ const Home = () => {
           <div className='block w-fit-content text-6xl relative text-transparent ml-6 md:text-7xl xl:text-8xl'>
             <h1 className='mb-8'>Hello, <span className='absolute top-0 left-0 h-full w-0'></span></h1>
             <h1 className='mb-8'>My Name is <span className='absolute top-0 left-0 h-full w-0'></span></h1>
-            <h1 className='mb-8'>Kenan Dolic <span className='absolute top-0 left-0 h-full w-0'></span></h1>
+            <h1 className='mb-8'>{props.data ? props.data.name : 'Loading..'}  <span className='absolute top-0 left-0 h-full w-0'></span></h1>
           </div>
             <h2 className='relative block w-fit-content  ml-6 text-5xl my-16'>
               <Typewriter
