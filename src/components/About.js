197359f6532fb4a36cec1d98b9ab7d66e555fd54
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-31 16:25:36 -0500

    portfolio update data - About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import resume from './../images/KDResume2021.pdf'
 
-const About = () => {
+const About = (props) => {
     return (
       <section id="about" className='flex justify-center'>
       <div className="flex-col-reverse text-center max-w-7xl my-auto min-h-screen flex items-center justify-center md:flex-row md:py-2 md:px-4 ">
@@ -12,8 +12,8 @@ const About = () => {
         </div>
         <div className='w-full px-16 text-center md:text-center md:p-20'>
           <h1 class="section-title text-5xl md:text-7xl xl:text-8xl">About <span>me</span></h1>
-          <h2 className='text-3xl font-medium tracking-widest mb-10'>Full-Stack Developer</h2>
-          <p className='text-center text-xl tracking-wider mb-20 md:text-2xl'>My name is Kenan. Currently a full-stack web development student at <span className='text-red-700 font-medium'>Lambda School</span>. I enjoy creating user-friendly interfaces and web applications. I am constantly experimenting with new technologies and always striving to improve as a developer and designer. When I'm not coding, I enjoy working out, hiking, gaming, and spending time with family or friends.</p>
+          <h2 className='text-3xl font-medium tracking-widest mb-10'>{props.data ? props.data.title : 'loading...'}</h2>
+          <p className='text-center text-xl tracking-wider mb-20 md:text-2xl'>{props.data ? props.data.paragraph : 'loading...'}</p>
           <a href={resume} download className='border-2 border-red-700 rounded-xl text-xl uppercase tracking-wider text-black hover:bg-red-700 hover:text-white transition ease-in p-3 md:text-2xl md:py-3 md:px-8 xl:text-3xl'>Download Resume</a>
         </div>
       </div>
