fe8550a037e247f175d8dc0e18431b14f8391b83
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-06-24 21:38:06 -0500

    updated About component information and resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
-import resume from './../images/KDResume.pdf'
+import resume from './../images/KDResume1.pdf'
 
 const About = (props) => {
     return (
